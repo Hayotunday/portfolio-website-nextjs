d7d90b2da2d9db29eab823ceff2b2369ccf280cb
Hayotunday <idowudanielayotunde@gmail.com>
2024-02-12 23:02:09 +0100

    Corrected ProjectSection import error

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import EmailSection from "@/components/EmailSection";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
-import ProjectSection from "@/components/Projectsection";
+import ProjectSection from "@/components/ProjectSection";
 import Image from "next/image";
 
 export default function Home() {
